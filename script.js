4c426591a8511666bea77f007d78722de52ee5e6
cyberDevansh <deepdive.devansh@gmail.com>
2025-04-17 12:14:30 +0530

    js basics

---
+++ script.js
@@ -0,0 +1,6 @@
+function changePara(){
+    document.getElementById("demo1").innerHTML = "Text changed by using the js in external file";
+}
+function sum(){
+    document.getElementById("demo2").innerHTML = 5 + 6;
+} 
